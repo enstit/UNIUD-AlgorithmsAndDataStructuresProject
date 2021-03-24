e8938cb0ae2db9ac766c886b3bffe94c1480308b
enstit <67797287+enstit@users.noreply.github.com>
2021-03-24 10:11:01 +0100

    Minor changes

--- sources/PeriodSmart.py
+++ sources/PeriodSmart.py
@@ -1,20 +1,13 @@
 # Returns the minimum fractional period of s
 def PeriodSmart(s):
     n = len(s)
-    pf = [None] * n
-    pf[0] = 0
+    pf = [0]
     for i in range (1, n):
         j = pf[i-1]
-        while j > 0 and s[i] != s[j]:
+        while ((j > 0) and (s[i] != s[j])):
             j = pf[j-1]
-        if s[i] == s[j]:
+        if (s[i] == s[j]):
             j += 1
-        pf[i] = j
+        pf.append(j)
     maxValue = pf[n-1]
     return n - maxValue
-
-# Returns True if string t is a Border of string s
-def isStringBorder(t, s):
-    n_t = len(t)
-    n_s = len(s)
-    return ((s[:n_t] == t) and (s[n_s-n_t:] == t))
