8f48c7c1f1f21ca4ca71bd5070a6d20a35c4945a
voodarkdoo <38936397+voodarkdoo@users.noreply.github.com>
2021-05-04 16:32:22 +0200

    Add new find and insertion methods

--- Seconda parte/sources/bst.py
+++ Seconda parte/sources/bst.py
@@ -26,6 +26,39 @@ def bst_insert(root, value, str_name):
 
     return root
 
+def bst_insert_iterative(root, value, str_name):
+    """
+    insert a key value in a tree
+    :param root: BSTNode object that represents
+        the root of the tree
+    :param value: an integer representing the value to insert
+    :param str_name: a string corresponding to
+        the literal format of the value
+    :return: a BSTNode object
+    """
+    
+    newnode = Node(value, str_name)
+ 
+    x = root
+    y = None
+ 
+    while (x != None):
+        y = x
+        if (value < x.key):
+            x = x.left
+        else:
+            x = x.right
+     
+    if (y == None):
+        y = newnode
+ 
+    elif (value < y.key):
+        y.left = newnode
+ 
+    else:
+        y.right = newnode
+    return y
+
 
 def bst_find(root, value):
     """
@@ -38,12 +71,30 @@ def bst_find(root, value):
         return
 
     if root.key == value:
-        print(root.name)
+        return root.name
 
     if root.key < value:
         return bst_find(root.right, value)
 
     return bst_find(root.left, value)
+
+def bst_find_iterative(root, value):
+    """
+    print the found value in a literal format
+    :param root: BSTNode object that represents
+        the root of the tree
+    :param value: an integer representing the value to find
+    """
+    x = root
+    while x is not None:
+        if x.key == value:
+            return x.name
+        if x.key < value:
+            x = x.right
+        else:
+            x = x.left
+    
+    return
 
 
 def bst_show(root):
